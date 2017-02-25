cddaff3bbfe71e43a773c119e1b3a16e45d74d59
Alex <tianyaqu@qq.com>
2017-02-26 00:15:37 +0800

    use async to make it really simutiously

--- main.py
+++ main.py
@@ -24,7 +24,7 @@ async def parseArticle(url):
     soup = BeautifulSoup(text,'html5lib')
     headline = soup.article.h1.text
     print('------------------')
-    print(headline)
+    print('title ',headline)
     for content in soup.article.select('.main-content'):
         print (content)
 
@@ -49,44 +49,15 @@ async def parseEdition(url):
 
 async def fetchEdition(rootUrl):
     u = await parseEdition(rootUrl)
-    for url in u:
-        await parseArticle(url)
-'''
+    tasks = [asyncio.ensure_future(parseArticle(url)) for url in u]
+    for task in tasks:
+        if task.done() == True:
+            task.cancel()
+
 def run():
-    proxies = {
-        'http': 'http://127.0.0.1:8087',
-    }
-
-    headers = {
-        'User-Agent':'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_12_2) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/54.0.2840.98 Safari/537.36',
-        'Referer':'http://www.economist.com/printedition/2017-02-18'
-    }
-
-    baseUrl = 'http://www.economist.com'
-    suffix = "/print"
-    urls = []
-    r = requests.get('http://www.economist.com/printedition/2017-02-18', headers=headers,proxies=proxies)
-    soup = BeautifulSoup(r.text,'html5lib')
-    for item in soup.body.select('.main-content .list__item'):
-        for child in item.children:
-            if child['class']:
-                sectionName = child['class'][0]
-                if sectionName == 'list__title':
-                    print 'sec ',child.text
-                else:
-                    #print child['href'],child.text
-                    urls.append(baseUrl + child['href'] + suffix)
-        print '---' 
-
-    for url in urls:
-        print url
-        text = fetch(url)
-        parseArticle(text)
-'''        
-if __name__ == '__main__':
-    #run()
+    rootUrl = 'http://www.economist.com/printedition/2017-02-18'
     loop = asyncio.get_event_loop()
-    tasks = [parseArticle(url) for url in ['http://www.economist.com/news/world-week/21717118-politics-week/print', 
-        'http://www.economist.com/news/britain/21717088-task-recovering-past-errors-goes-and-co-op-bank-puts-itself-up-sale/print', 
-        'http://www.economist.com/news/britain/21716932-word-warning-valentines-day-lovestruck-britons-are-losing-50m-year-dating-site/print']]
-    loop.run_until_complete(asyncio.wait([fetchEdition('http://www.economist.com/printedition/2017-02-18')]))
+    loop.run_until_complete(fetchEdition(rootUrl))
+
+if __name__ == '__main__':
+    run()
